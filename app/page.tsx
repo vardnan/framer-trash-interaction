96e458132ea51c1ab121ea23f99f32f39e7d2b8d
vardnan <vardnansivarajah@gmail.com>
2024-08-03 17:35:13 -0400

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { TrashAnimation } from './components/trash-animation';
+
+export default function Home() {
+  return (
+    <div className="h-screen items-center justify-center flex">
+      <TrashAnimation />
+    </div>
+  );
+}
